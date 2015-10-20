f24fc15290b48fc3fcb871c01718b0d5eac46bdb
Malcolm Jones <malcolm@adobe.com>
2015-10-20 14:03:02 -0400

    chg: initial commit basic dbus runners

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # scarlett-dbus-poc
-Scarlett Dbus Listener Service implementation Proof-Of-Concept. Source: https://www.reddit.com/r/gnome/comments/3owhp6/python_help_critique_my_application_design_home/
+
+Scarlett Dbus Listener Service implementation Proof-Of-Concept. 
+
+
+Source: https://www.reddit.com/r/gnome/comments/3owhp6/python_help_critique_my_application_design_home/
+
+
+### Simple python dbus example
+
+Source: https://github.com/stylesuxx/python-dbus-examples
+
+* Run dbus monitor and grep for the interesting output:
+
+    dbus-monitor | grep /tld/domain/sub
+
+* Run the receiver
+
+Then you can either
+* Run the invoker, which will call the proxxied receivers Test object methods.
+* run the emitter, which will emit a test signal and a quit signal.
+
+After running either of them, the receiver should be stopped.
---
+++ receiver.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import gobject
+import dbus
+import dbus.service
+import dbus.glib
+
+class Test(dbus.service.Object):
+  loop = None
+  def __init__(self, bus_name, object_path, loop):
+    dbus.service.Object.__init__(self, bus_name, object_path)
+    self.loop = loop
+
+  @dbus.service.method('tld.domain.sub.TestInterface')
+  def foo(self):
+    return "Foo"
+
+  # Stop the main loop
+  @dbus.service.method('tld.domain.sub.TestInterface')
+  def stop(self):
+    self.loop.quit()
+    return "Quit loop"
+
+  # Pass an exception through dbus
+  @dbus.service.method('tld.domain.sub.TestInterface')
+  def fail(self):
+    raise Exception, 'FAIL!'
+
+def catchall_handler(*args, **kwargs):
+    print ("Caught signal: "
+           + kwargs['dbus_interface'] + "." + kwargs['member'])
+    for arg in args:
+        print "        " + str(arg)
+
+def quit_handler():
+  print "Quitting...."
+  loop.quit()
+
+"""
+Register to dbus and run the main loop.
+Will provide the Test object via dbus.
+"""
+loop = gobject.MainLoop()
+session_bus = dbus.SessionBus()
+
+bus_name = dbus.service.BusName('sub.domain.tld', bus = session_bus)
+obj = Test(bus_name, '/tld/domain/sub/test', loop)
+session_bus.add_signal_receiver(catchall_handler, interface_keyword='dbus_interface', member_keyword='member')
+session_bus.add_signal_receiver(quit_handler, dbus_interface = "tld.domain.sub.event", signal_name = "quit_signal")
+
+loop.run()
