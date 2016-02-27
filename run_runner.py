6f0d9a4247dac4c4f6f3d6c82810346605bc0adc
Malcolm Jones <malcolm@adobe.com>
2016-02-27 16:14:42 -0500

    chg: cleanup run_runner.py

--- run_runner.py
+++ run_runner.py
@@ -132,72 +132,6 @@ def trace(func):
 
 NUM_THREADS = 10
 
-# class WorkerThread(threading.Thread):
-#     @trace
-#     def __init__(self, q):
-#         global logger
-#         super(WorkerThread, self).__init__()
-#         self.q = q
-#         self.exception = None
-#
-#     @trace
-#     def run(self):
-#         try:
-#             while not self.q.empty():
-#                 item = None
-#
-#                 try:
-#                     item = self.q.get(False)
-#                     # do your stuff
-#                     print 'HERE WE GO: ' + str(item) + '\n'
-#                 except Queue.Empty:
-#                     pass
-#                 finally:
-#                     if item:
-#                         self.q.task_done()
-#
-#         except Exception as e:
-#             self.exception = e
-#
-#     @trace
-#     def get_exception(self):
-#         return self.exception
-#
-#
-# class MyClass:
-#     global logger
-#     @trace
-#     def spawn_threads(self, q):
-#         self.threads = []
-#
-#         for _ in range(NUM_THREADS):
-#             t = WorkerThread(q)
-#             self.threads.append(t)
-#
-#             t.start()
-#
-#     @trace
-#     def raise_thread_exceptions(self):
-#         for t in self.threads:
-#             e = t.get_exception()
-#
-#             if e:
-#                 raise e
-#
-#     @trace
-#     def my_func(self):
-#         q = Queue.Queue()
-#
-#         # do something and q.put()
-#         _temp_val = randint(0, 9)
-#         q.put(_temp_val)
-#
-#         self.spawn_threads(q)
-#         q.join()
-#
-#         # raise exceptions from worker threads
-#         self.raise_thread_exceptions()
-
 
 class ExcThread(threading.Thread):
     """
@@ -228,6 +162,11 @@ def main():
     """
     bucket = Queue.Queue()
 
+    # TODO: Try calling child thread like below.
+    # TODO: Allow us to pass in a target, and args.
+    # TODO: Eg. target=ScarlettPlayer or target=ScarlettSpeaker
+    # SOURCE: https://github.com/jhcepas/npr/blob/master/nprlib/interface.py
+    # t = ExcThread(bucket=exceptions, target=func, args=[args])
     # Start child thread
     thread_obj = ExcThread(bucket)
     thread_obj.daemon = True
