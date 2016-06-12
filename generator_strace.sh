b6776e98a4ecf94eb768fc8ec4d570518e685ce6
Malcolm Jones <malcolm@adobe.com>
2016-06-12 12:36:48 -0400

    update: strace shell script fixed

--- generator_strace.sh
+++ generator_strace.sh
@@ -9,19 +9,28 @@
 #
 #
 mkdir -p /var/log/scarlett_debug
-_PROCESS="generator_${1}"
-echo "Looking for Process: ${_PROCESS}"
+
+_ARG=$1
+
+if [[ "${_ARG}" = "" ]]; then
+   echo "_PROCESS is set to '$_ARG'"
+   echo "Please pass in an argument to use this script eg. [tasker, listener, player, speaker]"
+   exit 1
+fi
+
+_PROCESS="generator_${_ARG}"
+
 for m in {1..4000}
 do
   _PID_LIST=$(ps -ef|grep ${_PROCESS}|grep -v grep|awk '{print $2}'|xargs)
   echo "Pid list: $_PID_LIST"
   for i in $_PID_LIST
   do
-    if [[ -f /var/log/scarlett_debug/generator_$1.out ]]
+    if [[ -f /var/log/scarlett_debug/${i}_${_PROCESS}.out ]]
     then
       echo "skipping strace $i"
     else
-      strace -o /var/log/scarlett_debug/generator_$1.out -s 40000 -vvtf -p $i &
+      strace -o /var/log/scarlett_debug/${i}_${_PROCESS}.out -s 40000 -vvtf -p $i &
     fi
   done
   sleep 1
